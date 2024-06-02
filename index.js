02767bca70fad349fed6aa24594f907fd66a36b2
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-06-02 08:26:52 +0530

    add cors

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+
+app.use(cors({ origin: "https://resume-builder-sigma-five.vercel.app/" }));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/resumeBuilder', express.static(path.join(__dirname, 'resumeBuilder')));
